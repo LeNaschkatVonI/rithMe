0ba5133cf8675a266d19e9701a3e675bb844430d
Maxim Vainikka <rithmique@yahoo.com>
2021-05-28 08:48:35 +0300

    Initial Commit

---
+++ rithMe/rithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  rithMeApp.swift
+//  rithMe
+//
+//  Created by Максим Нуждин on 28.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct rithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
